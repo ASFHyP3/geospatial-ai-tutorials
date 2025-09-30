6f63d921911fb1ba44bf62eea52e1ae7d4da996e
jacquelynsmale <jacquelyn.smale@gmail.com>
2025-09-30 10:20:39 -0800

    update RGB indicies

--- terramind/README.md
+++ terramind/README.md
@@ -8,10 +8,12 @@ When downloading data, make sure to download in the following structure:
     │   └── *_S2L1C.zarr.zip files
     ├── S1RTC
     │   └── *_S1RTC.zarr.zip files
-    ├── Label
-    │   └── *_Label.zarr.zip files
-    └── splits
-        ├── test_data.txt
-        ├── train_data.txt
-        └── valid_data
+    └── Label
+        └── *_Label.zarr.zip files
+    splits
+    ├── test_data.txt 
+    ├── train_data.txt
+    └── valid_data
+
+The `splits` files should include the base names of the files you wish to include (e.g. `swathID_XXX_swathDate_YYYY-MM-DD`)
 
--- terramind/load_chips.py
+++ terramind/load_chips.py
@@ -4,8 +4,7 @@ import albumentations
 import matplotlib.pyplot as plt
 import terratorch
 
-
-dataset_path = Path('terramind-docs/terramind')
+dataset_path = Path('terramind')
 
 datamodule = terratorch.datamodules.GenericMultiModalDataModule(
     task="segmentation",
@@ -15,7 +14,7 @@ datamodule = terratorch.datamodules.GenericMultiModalDataModule(
     # Define your input modalities. The names must match the keys in the following dicts.
     modalities=["S2L1C", "S1RTC"],
     rgb_modality="S2L1C",  # Used for plotting. Defaults to the first modality if not provided.
-    rgb_indices=[3, 2, 1],  # RGB channel positions in the rgb_modality.
+    rgb_indices=[6, 2, 0],  # RGB channel positions in the rgb_modality.
 
     # Define data paths as dicts using the modality names as keys.
     train_data_root={
@@ -59,7 +58,7 @@ datamodule = terratorch.datamodules.GenericMultiModalDataModule(
     means={
         "S2L1C": [2357.089, 2137.385, 2018.788, 2082.986, 2295.651, 2854.537, 3122.849, 3040.560, 3306.481, 1473.847,
                   506.070, 2472.825, 1838.929],
-        "S2L2A": [1390.458, 1503.317, 1718.197, 1853.910, 2199.100, 2779.975, 2987.011, 3083.234, 3132.220, 3162.988,
+        "S2L1C": [1390.458, 1503.317, 1718.197, 1853.910, 2199.100, 2779.975, 2987.011, 3083.234, 3132.220, 3162.988,
                   2424.884, 1857.648],
         "S1GRD": [-12.599, -20.293],
         "S1RTC": [-10.93, -17.329],
@@ -69,7 +68,7 @@ datamodule = terratorch.datamodules.GenericMultiModalDataModule(
     stds={
         "S2L1C": [1624.683, 1675.806, 1557.708, 1833.702, 1823.738, 1733.977, 1732.131, 1679.732, 1727.26, 1024.687,
                   442.165, 1331.411, 1160.419],
-        "S2L2A": [2106.761, 2141.107, 2038.973, 2134.138, 2085.321, 1889.926, 1820.257, 1871.918, 1753.829, 1797.379,
+        "S2L1C": [2106.761, 2141.107, 2038.973, 2134.138, 2085.321, 1889.926, 1820.257, 1871.918, 1753.829, 1797.379,
                   1434.261, 1334.311],
         "S1GRD": [5.195, 5.890],
         "S1RTC": [4.391, 4.459],
@@ -93,9 +92,10 @@ datamodule.setup("fit")
 
 val_dataset = datamodule.val_dataset
 len(val_dataset)
+print(val_dataset)
 
 # plotting a few samples (The code only plots the defined `rgb_modality`)
-val_dataset.plot(val_dataset[2])
+val_dataset.plot(val_dataset[0])
 plt.show()
 val_dataset.plot(val_dataset[8])
 plt.show()
