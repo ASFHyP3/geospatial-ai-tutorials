bf284095a09be3746fc25b5ae577cfdbe833e5d5
jacquelynsmale <jacquelyn.smale@gmail.com>
2025-10-01 13:32:47 -0800

    fix naming from S2L1C to S2L2A

--- terramind/load_chips.py
+++ terramind/load_chips.py
@@ -4,7 +4,7 @@ import albumentations
 import matplotlib.pyplot as plt
 import terratorch
 
-dataset_path = Path('terramind')
+dataset_path = Path('')
 
 datamodule = terratorch.datamodules.GenericMultiModalDataModule(
     task="segmentation",
@@ -12,23 +12,23 @@ datamodule = terratorch.datamodules.GenericMultiModalDataModule(
     num_workers=2,
     num_classes=2,
     # Define your input modalities. The names must match the keys in the following dicts.
-    modalities=["S2L1C", "S1RTC"],
-    rgb_modality="S2L1C",  # Used for plotting. Defaults to the first modality if not provided.
+    modalities=["S2L2A", "S1RTC"],
+    rgb_modality="S2L2A",  # Used for plotting. Defaults to the first modality if not provided.
     rgb_indices=[6, 2, 0],  # RGB channel positions in the rgb_modality.
 
     # Define data paths as dicts using the modality names as keys.
     train_data_root={
-        "S2L1C": dataset_path / 'data/S2L1C',
+        "S2L2A": dataset_path / 'data/S2L2A',
         "S1RTC": dataset_path / 'data/S1RTC',
     },
     train_label_data_root=dataset_path / 'data/Label',
     val_data_root={
-        "S2L1C": dataset_path / 'data/S2L1C',
+        "S2L2A": dataset_path / 'data/S2L2A',
         "S1RTC": dataset_path / 'data/S1RTC',
     },
     val_label_data_root=dataset_path / 'data/Label',
     test_data_root={
-        "S2L1C": dataset_path / 'data/S2L1C',
+        "S2L2A": dataset_path / 'data/S2L2A',
         "S1RTC": dataset_path / 'data/S1RTC',
     },
     test_label_data_root=dataset_path / 'data/Label',
@@ -40,7 +40,7 @@ datamodule = terratorch.datamodules.GenericMultiModalDataModule(
 
     # Define suffix, again using dicts.
     img_grep={
-        "S2L1C": "*_S2L2A.zarr.zip",
+        "S2L2A": "*_S2L2A.zarr.zip",
         "S1GRD": "*_S1RTC.zarr.zip",
     },
     label_grep="*_Label.zarr.zip",
@@ -58,7 +58,7 @@ datamodule = terratorch.datamodules.GenericMultiModalDataModule(
     means={
         "S2L1C": [2357.089, 2137.385, 2018.788, 2082.986, 2295.651, 2854.537, 3122.849, 3040.560, 3306.481, 1473.847,
                   506.070, 2472.825, 1838.929],
-        "S2L1C": [1390.458, 1503.317, 1718.197, 1853.910, 2199.100, 2779.975, 2987.011, 3083.234, 3132.220, 3162.988,
+        "S2L2A": [1390.458, 1503.317, 1718.197, 1853.910, 2199.100, 2779.975, 2987.011, 3083.234, 3132.220, 3162.988,
                   2424.884, 1857.648],
         "S1GRD": [-12.599, -20.293],
         "S1RTC": [-10.93, -17.329],
@@ -68,7 +68,7 @@ datamodule = terratorch.datamodules.GenericMultiModalDataModule(
     stds={
         "S2L1C": [1624.683, 1675.806, 1557.708, 1833.702, 1823.738, 1733.977, 1732.131, 1679.732, 1727.26, 1024.687,
                   442.165, 1331.411, 1160.419],
-        "S2L1C": [2106.761, 2141.107, 2038.973, 2134.138, 2085.321, 1889.926, 1820.257, 1871.918, 1753.829, 1797.379,
+        "S2L2A": [2106.761, 2141.107, 2038.973, 2134.138, 2085.321, 1889.926, 1820.257, 1871.918, 1753.829, 1797.379,
                   1434.261, 1334.311],
         "S1GRD": [5.195, 5.890],
         "S1RTC": [4.391, 4.459],
