cf1958c32adc777854f5c29827e1ca6e21e81965
William Horn <wbhorn@alaska.edu>
2025-10-17 10:35:57 -0800

    Remove squeeze flag in _get_image_array

--- severe-weather/loader.py
+++ severe-weather/loader.py
@@ -74,7 +74,7 @@ class SatChipDataset(NonGeoDataset):
 
     def __getitem__(self, index: int) -> dict[str, Any]:
         chip_paths = self.chip_list[index]
-        label = self._get_image_array(chip_paths['LABEL'], squeeze=True)
+        label = self._get_image_array(chip_paths['LABEL']).squeeze()
         image_output = {
             'S1RTC': self._get_image_array(chip_paths['S1RTC']),
             'S2L2A': self._get_image_array(chip_paths['S2L2A']),
@@ -82,14 +82,11 @@ class SatChipDataset(NonGeoDataset):
         output = {'mask': label, 'image': image_output}
         return output
 
-    def _get_image_array(self, chip_path: Path, squeeze=False) -> torch.Tensor:
+    def _get_image_array(self, chip_path: Path) -> torch.Tensor:
         ds = self._load_ds(chip_path)
         array = ds.bands.isel(time=0).data.astype(np.float32)
         array = np.transpose(array, (1, 2, 0))  # to height, width, channel
-        tensor = self.transforms(image=array)['image']
-        if squeeze:
-            tensor = tensor.squeeze()
-        return tensor
+        return self.transforms(image=array)['image']
 
     def _load_ds(self, dataset_path: str | Path) -> xr.Dataset:
         store = zarr.storage.ZipStore(dataset_path, read_only=True)  # type: ignore
