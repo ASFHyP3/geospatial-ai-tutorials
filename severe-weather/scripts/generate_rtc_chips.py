157faed0d801e799ff31db6733f76f6a326025da
William Horn <wbhorn@alaska.edu>
2026-02-11 15:40:15 -0900

    Remove unused MASK

--- severe-weather/scripts/generate_rtc_chips.py
+++ severe-weather/scripts/generate_rtc_chips.py
@@ -45,14 +45,13 @@ def main():
 
     earthaccess.login()
 
-    # chip_swaths(gdf, data_paths)
+    chip_swaths(gdf, data_paths)
 
     for _, swath in gdf.iterrows():
         swath_id = _make_swath_id(swath["swathID"])
 
         all_chips = list(data_paths["CHIPS"].glob(f"{swath_id}.*.tif"))
         good_chips = list(data_paths["CHIPS_TM"].glob(f"{swath_id}.*.tif"))
-
         merged_file = list(data_paths["MERGE"].glob(f"{swath_id}.*BANDS.tif"))[0]
 
         _plot_chips(merged_file, all_chips, good_chips, swath)
@@ -181,14 +180,13 @@ def _get_data_for_swath(swath: pd.Series, data_paths: dict) -> dict[str, Path] |
 
         merged[band] = merged_band_path
 
-    event_tif, mask_tif = _generate_masks(
+    event_tif = _generate_masks(
         merged["VV"], swath, merged_extension="VV.tif"
     )
 
     return {
         **merged,
         "EVENT": event_tif,
-        "MASK": mask_tif,
     }
 
 
@@ -326,21 +324,9 @@ def _generate_masks(
 ) -> tuple[Path, Path]:
 
     event_path = _rename(merged_path, merged_extension, "EVENT.tif")
-    mask_path = _rename(merged_path, merged_extension, "MASK.tif")
 
     with rasterio.open(merged_path) as ds:
         profile = ds.profile
-
-        mask_raster = features.rasterize(
-            shapes=[[swath["geometry"], 1]],
-            fill=0,
-            out_shape=ds.shape,
-            transform=ds.transform,
-        )
-
-        with rasterio.open(mask_path, "w", **profile) as dst:
-            dst.write(mask_raster, 1)
-            print("generated:", mask_path)
 
         event_mask = features.rasterize(
             shapes=[
@@ -357,7 +343,7 @@ def _generate_masks(
             dst.write(event_mask, 1)
             print("generated:", event_path)
 
-    return event_path, mask_path
+    return event_path
 
 
 def _stack_rtc_bands(merged: dict[str, Path], data_bands: tuple[str]) -> None:
@@ -379,7 +365,6 @@ def _stack_rtc_bands(merged: dict[str, Path], data_bands: tuple[str]) -> None:
 
 def _chip_rtc_data(merged: dict[str, Path], data_paths: dict[str, Path], chip_size=256):
     chips = {}
-    # chips[tile_id] = {band, event, mask}
 
     grid = []
     with rasterio.open(merged["BANDS"]) as ref:
@@ -395,8 +380,7 @@ def _chip_rtc_data(merged: dict[str, Path], data_paths: dict[str, Path], chip_si
                 chips[tile_id] = {}
                 grid.append((tile_id, bounds))
 
-    # BAND (VV, VH), EVENT, MASK
-    for chip_layer in ("BANDS", "mask", "EVENT", "MASK"):
+    for chip_layer in ("BANDS", "EVENT"):
         layer_path = merged[chip_layer]
 
         with rasterio.open(layer_path) as src:
